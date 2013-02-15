af5abeb4fe263d1580a69951f8c7a75102b4b296
Jan Marthedal Rasmussen <jmr@kanooth.com>
2013-02-15 14:32:49 +0100

    Test user 1 A few more items make_fixtures script

---
+++ make_fixtures.sh
@@ -0,0 +1,5 @@
+python manage.py dumpdata items > items/fixtures/initial_data.json
+python manage.py dumpdata refs > refs/fixtures/initial_data.json
+python manage.py dumpdata tags > tags/fixtures/initial_data.json
+python manage.py dumpdata users > users/fixtures/initial_data.json
+python manage.py dumpdata validate > validate/fixtures/initial_data.json
