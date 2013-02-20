caee6946d42d1bfeff6d716e1b67b23cbc918e6f
Jan Marthedal Rasmussen <jmr@kanooth.com>
2013-02-20 10:46:20 +0100

    some more items clean_tag unittest

--- items/tests.py
+++ items/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from items.models import final_id_to_name, final_name_to_id
 
-class FinalIdTest(TestCase):
+class PublicIdTest(TestCase):
 
     def test_one(self):
         for exp in range(0, 20):
--- tags/tests.py
+++ tags/tests.py
@@ -1,16 +1,7 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from tags.helpers import clean_tag
 
+class CleanTagTest(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_one(self):
+        self.assertEqual("abc de", clean_tag("abc  de "))
