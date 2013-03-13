a8b9241a0405135a9136907be2df13ac0fc8f860
Jan Marthedal Rasmussen <jmr@kanooth.com>
2013-03-13 07:36:26 +0100

    Fixture pretty print

--- make_fixtures.sh
+++ make_fixtures.sh
@@ -1,4 +1,4 @@
-python manage.py dumpdata items > items/fixtures/initial_data.json
-python manage.py dumpdata refs > refs/fixtures/initial_data.json
-python manage.py dumpdata tags > tags/fixtures/initial_data.json
-python manage.py dumpdata users > users/fixtures/initial_data.json
+python manage.py dumpdata items | python -mjson.tool > items/fixtures/initial_data.json
+python manage.py dumpdata refs | python -mjson.tool > refs/fixtures/initial_data.json
+python manage.py dumpdata tags | python -mjson.tool > tags/fixtures/initial_data.json
+python manage.py dumpdata users | python -mjson.tool > users/fixtures/initial_data.json
