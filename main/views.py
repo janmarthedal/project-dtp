019dc4e601121d38b128dd243323b5159aa24a6c
Jan Marthedal Rasmussen <jmr@kanooth.com>
2013-04-19 14:48:15 +0200

    New front page layout Blog feed reading script

--- main/views.py
+++ main/views.py
@@ -4,6 +4,8 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.conf import settings
 from main.helpers import init_context
+from items.models import FinalItem, DraftItem
+from users.models import User
 
 @require_safe
 def index(request):
@@ -13,7 +15,17 @@ def index(request):
 
 @require_safe
 def home(request):
-    c = init_context('home') 
+    c = init_context('home')
+    c['def_final'] = FinalItem.objects.filter(itemtype='D', status='F').count()
+    c['thm_final'] = FinalItem.objects.filter(itemtype='T', status='F').count()
+    c['prf_final'] = FinalItem.objects.filter(itemtype='P', status='F').count()
+    c['def_review'] = DraftItem.objects.filter(itemtype='D', status='R').count()
+    c['thm_review'] = DraftItem.objects.filter(itemtype='T', status='R').count()
+    c['prf_review'] = DraftItem.objects.filter(itemtype='P', status='R').count()
+    c['def_draft'] = DraftItem.objects.filter(itemtype='D', status='D').count()
+    c['thm_draft'] = DraftItem.objects.filter(itemtype='T', status='D').count()
+    c['prf_draft'] = DraftItem.objects.filter(itemtype='P', status='D').count()
+    c['user_count'] = User.objects.filter(is_active=True).count()
     return render(request, 'home.html', c)
 
 @require_safe
---
+++ scripts/get_blog_feed.py
@@ -0,0 +1,17 @@
+import urllib
+import xml.etree.ElementTree as ET
+
+feed = urllib.urlopen('http://blog.teoremer.com/feed').read()
+root = ET.fromstring(feed)
+channel = root.find('channel')
+
+result = []
+
+for item in channel.findall('item'):
+    result.append({
+                   'title':       item.find('title').text,
+                   'link':        item.find('link').text,
+                   'description': item.find('description').text
+                   })
+
+print result
