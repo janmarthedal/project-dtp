1202aab8971861ec575823c96ee6801129915143
Jan Marthedal Rasmussen <jmr@kanooth.com>
2013-09-10 15:03:30 +0200

    Removed old add source

--- sources/urls.py
+++ sources/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('sources.views',
-    url(r'^$',          'index'),
-    url(r'^add/(\w+)$', 'add_source'),
-    url(r'^add2$',      'add_source2')
+    url(r'^$',    'index'),
+    url(r'^add$', 'add_source')
 )
 
