a20c8f978b9fdc4e64f6f77b52d21f0e65b865ad
Jan Marthedal Rasmussen <jmr@kanooth.com>
2014-01-29 19:43:44 +0100

    Issue 22: Recompute points command

--- analysis/management/commands/points.py
+++ analysis/management/commands/points.py
@@ -1,13 +1,58 @@
 from django.core.management.base import BaseCommand
 from django.db.models import Sum
 from analysis.helpers import queryset_generator
-from items.models import ItemValidation
+from items.models import FinalItem, ItemValidation
 
 class Command(BaseCommand):
     help = 'Recomputes all item/validation points'
 
     def handle(self, *args, **options):
-        # TODO: Can we do better?
-        for iv in queryset_generator(ItemValidation.objects.annotate(sub_points=Sum('useritemvalidation__value'))):
-            iv.points = iv.sub_points or 0
-            iv.save()
+        total = 0
+        changed = 0
+        for item in queryset_generator(ItemValidation.objects.annotate(validation_points=Sum('useritemvalidation__value'))):
+            total += 1
+            new_points = item.validation_points or 0
+            if new_points != item.points:
+                changed += 1
+                item.points = new_points
+                item.save()
+        self.stdout.write('{} validations, {} points changed'.format(total, changed))
+
+        total = 0
+        changed = 0
+        for item in queryset_generator(FinalItem.objects.filter(status='F', itemtype='P')):
+            total += 1
+            sum_aggregate = ItemValidation.objects.filter(item=item, points__gt=0).aggregate(Sum('points'))
+            new_points = 1 + (sum_aggregate['points__sum'] or 0)
+            if new_points != item.points:
+                changed += 1
+                item.points = new_points
+                item.save()
+        self.stdout.write('{} proofs, {} points changed'.format(total, changed))
+
+        total = 0
+        changed = 0
+        for item in queryset_generator(FinalItem.objects.filter(status='F', itemtype='T')):
+            total += 1
+            new_points = 1
+            sum_aggregate = ItemValidation.objects.filter(item=item, points__gt=0).aggregate(Sum('points'))
+            new_points += sum_aggregate['points__sum'] or 0
+            sum_aggregate = FinalItem.objects.filter(parent=item, status='F', itemtype='P', points__gt=0).aggregate(Sum('points'))
+            new_points += sum_aggregate['points__sum'] or 0
+            if new_points != item.points:
+                changed += 1
+                item.points = new_points
+                item.save()
+        self.stdout.write('{} theorems, {} points changed'.format(total, changed))
+
+        total = 0
+        changed = 0
+        for item in queryset_generator(FinalItem.objects.filter(status='F', itemtype='D')):
+            total += 1
+            sum_aggregate = ItemValidation.objects.filter(item=item, points__gt=0).aggregate(Sum('points'))
+            new_points = 1 + (sum_aggregate['points__sum'] or 0)
+            if new_points != item.points:
+                changed += 1
+                item.points = new_points
+                item.save()
+        self.stdout.write('{} definitions, {} points changed'.format(total, changed))
