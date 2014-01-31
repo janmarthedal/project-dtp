ad098befe12408166c468a59705df2d4bac4884f
Jan Marthedal Rasmussen <jmr@kanooth.com>
2014-01-31 13:59:13 +0100

    Show current category when viewing definitions in a certain category

--- tags/views.py
+++ tags/views.py
@@ -12,24 +12,28 @@ logger = logging.getLogger(__name__)
 def category_link_from_tags(tags):
     return reverse('tags.views.browse', args=['/'.join(tags)])
 
+def path_to_category_items(path):
+    tags = path.split('/') if path else []
+    return [{'name': tags[i], 'link': category_link_from_tags(tags[0:i])} for i in range(0, len(tags))]
+
 @require_safe
 def browse(request, path):
+    category_items = path_to_category_items(path)
     tags = path.split('/') if path else []
-    category_items = [dict(name = tags[i], link = category_link_from_tags(tags[0:i])) for i in range(0, len(tags))]
     category = Category.objects.from_tag_names_or_404(tags)
     listing = Category.objects.filter(parent=category).order_by('tag__name').values_list('tag__name', flat=True)
-    result_list = [dict(name = name, link = category_link_from_tags(tags + [name])) for name in listing]
-    c = init_context('categories', category_items = category_items, result_list = result_list, path = path)
+    result_list = [{'name': name, 'link': category_link_from_tags(tags + [name])} for name in listing]
+    c = init_context('categories', category_items=category_items, result_list=result_list, path = path)
     return render(request, 'tags/browse.html', c)
 
 @require_safe
 def list_definitions(request, path):
     if not path:
         raise Http404
+    category_items = path_to_category_items(path)
     tags = map(urllib.unquote, path.split('/'))
     logger.debug(tags)
     category = Category.objects.from_tag_names_or_404(tags)
-    c = init_context('definitions',
-                     init_items = item_search_to_json(itemtype='D', category=category),
-                     category = category.pk)
+    c = init_context('definitions', category_items=category_items, category=category.pk,
+                     init_items=item_search_to_json(itemtype='D', category=category))
     return render(request, 'tags/list_definitions.html', c)
