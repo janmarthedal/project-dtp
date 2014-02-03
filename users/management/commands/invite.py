dfe3c58b06b4d3dec82d49013e7339b68ef19091
Jan Marthedal Rasmussen <jmr@kanooth.com>
2014-02-03 19:56:19 +0100

    Invites accepts empty email, in which case no invite email is sent

--- users/management/commands/invite.py
+++ users/management/commands/invite.py
@@ -2,10 +2,11 @@ from django.core import management
 from users.models import Invitations
 
 class Command(management.base.BaseCommand):
-    args = 'target-email [target-name]'
+    args = '[target-email [target-name]]'
     help = 'Create and send an invitation by email'
 
-    def handle(self, target_email, target_name=None, **options):
+    def handle(self, target_email=None, target_name=None, **options):
         invite = Invitations.objects.make_invitation(target_email=target_email, target_name=target_name)
         self.stdout.write('Invitation created: {}'.format(invite))
-        invite.send()
+        if target_email:
+            invite.send()
