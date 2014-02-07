00023df4427facef2b9ca43cfca9851a218a9a0e
Jan Marthedal Rasmussen <jmr@kanooth.com>
2014-02-07 10:30:43 +0100

    Maintenance script: log file outside root directory

--- dev/scripts/maintenance.sh
+++ dev/scripts/maintenance.sh
@@ -2,14 +2,15 @@
 
 PYTHON=/usr/local/bin/python2.7
 export PYTHONPATH=/home/jmr/webapps/thrms_django/teoremer:/home/jmr/lib/python2.7
+LOG=/home/jmr/webapps/thrms_django/log/cron.log
 
 cd /home/jmr/webapps/thrms_django/teoremer
-echo >> log/cron.log
-echo "Maintenance `date`" >> log/cron.log
-echo "* clearsessions" >> log/cron.log
-$PYTHON manage.py clearsessions 2>&1 >> log/cron.log
-echo "* deps" >> log/cron.log
-$PYTHON manage.py deps 2>&1 >> log/cron.log
-echo "* points" >> log/cron.log
-$PYTHON manage.py points 2>&1 >> log/cron.log
-echo "Maintenance done" >> log/cron.log
+echo >> $LOG
+echo "Maintenance `date`" >> $LOG
+echo "* clearsessions" >> $LOG
+$PYTHON manage.py clearsessions 2>&1 >> $LOG
+echo "* deps" >> $LOG
+$PYTHON manage.py deps 2>&1 >> $LOG
+echo "* points" >> $LOG
+$PYTHON manage.py points 2>&1 >> $LOG
+echo "Maintenance done" >> $LOG
