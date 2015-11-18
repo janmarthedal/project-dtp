db1c32a50bd9e87f184969ae59be774459df3090
Jan Marthedal Rasmussen <jan@janmr.com>
2015-11-18 10:09:53 +0100

    Exclude unused apps, middleware

--- project/settings.py
+++ project/settings.py
@@ -10,24 +10,24 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost']
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
+#    'django.contrib.admin',
+#    'django.contrib.auth',
+#    'django.contrib.contenttypes',
+#    'django.contrib.sessions',
+#    'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
+#    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+#    'django.middleware.csrf.CsrfViewMiddleware',
+#    'django.contrib.auth.middleware.AuthenticationMiddleware',
+#    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+#    'django.contrib.messages.middleware.MessageMiddleware',
+#    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'project.urls'
--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import include, url
-from django.contrib import admin
 import main.views
 
 urlpatterns = [
     url(r'^create$', main.views.create),
-    url(r'^admin/', include(admin.site.urls)),
 ]
