13d71a955a6627900941cad905e44e0689079965
Jan Marthedal Rasmussen <jan@janmr.com>
2015-11-18 10:48:38 +0100

    Input/preview fields

--- project/settings.py
+++ project/settings.py
@@ -62,8 +62,8 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
+USE_I18N = False
+USE_L10N = False
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
