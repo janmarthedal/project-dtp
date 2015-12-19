305ea9f7e3bcff826ecdef6c5c540640ed5ca6b6
Jan Marthedal Rasmussen <jan@janmr.com>
2015-12-19 16:24:45 +0100

    Basic render with MathJax

--- src/render-item-box.js
+++ src/render-item-box.js
@@ -5,18 +5,65 @@ marked.setOptions({
   sanitize: true,
 });
 
+var img_re = new RegExp('<img src="([^"]*)".*?>', 'g');
+
+function prepareEquations(eqns) {
+    var peqns = {};
+    for (let key in eqns) {
+        let eqn = eqns[key];
+        peqns[key] = {
+            html: '<script type="math/tex' + (eqn.block ? '; mode=display' : '') +
+                '">' + eqn.source + '</script>',
+            mathjax: true
+        };
+    }
+    return peqns;
+}
+
+function dataItemToHtml(data) {
+    var eqns = prepareEquations(data.eqns || {}),
+        html = marked(data.body),
+        mathjax = false;
+    html = html.replace(img_re, function (_, m) {
+        if (m.indexOf('eqn/') == 0) {
+            let eqn = eqns[m.substring(4)];
+            if (eqn) {
+                mathjax = mathjax || eqn.mathjax;
+                return eqn.html;
+            }
+        }
+        return '<em>error</em>';
+    });
+    return {html: html, mathjax: mathjax};
+}
+
 export default class extends React.Component {
     constructor(props) {
         super(props);
         this.state = props.data || {body: ''};
+        this.updateMathJax = false;
     }
     setItemData(data) {
         this.setState(data);
     }
+    componentDidMount() {
+        this.postRender();
+    }
+    componentDidUpdate() {
+        this.postRender();
+    }
+    postRender() {
+        if (this.updateMathJax) {
+            window.MathJax.Hub.Queue(['Typeset', MathJax.Hub, this.refs.viewer]);
+            this.updateMathJax = false;
+        }
+    }
     render() {
-        var markup = {__html: marked(this.state.body)};
+        var output = dataItemToHtml(this.state);
+        var markup = {__html: output.html};
+        this.updateMathJax = output.mathjax;
         return (
-            <div className="item-view" dangerouslySetInnerHTML={markup} />
+            <div ref='viewer' className="item-view" dangerouslySetInnerHTML={markup} />
         );
     }
 }
