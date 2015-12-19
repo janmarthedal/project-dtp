2f719dc5ea0afb2913accd387f95005ea6c48851
Jan Marthedal Rasmussen <jan@janmr.com>
2015-12-19 09:50:26 +0100

    Simple markdown

--- src/render-item-box.js
+++ src/render-item-box.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import marked from 'marked';
+
+marked.setOptions({
+  sanitize: true,
+});
 
 export default React.createClass({
     getInitialState: function() {
@@ -8,8 +13,9 @@ export default React.createClass({
         this.setState({body: value});
     },
     render: function() {
+        var markup = {__html: marked(this.state.body)};
         return (
-            <div className="item-view">{this.state.body}</div>
+            <div className="item-view" dangerouslySetInnerHTML={markup} />
         );
     }
 });
