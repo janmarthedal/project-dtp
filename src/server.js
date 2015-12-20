57df595469db4d11a944e639a3725ff28fa70bfe
Jan Marthedal Rasmussen <jan@janmr.com>
2015-12-20 11:14:42 +0100

    Home page, create definition/theorem

--- src/server.js
+++ src/server.js
@@ -9,7 +9,8 @@ import RenderItemBox from './render-item-box';
 import {textToItemData} from './item-data';
 
 var app = express(),
-    base_dir = __dirname + '/..';
+    base_dir = __dirname + '/..',
+    create_types = {'definition': 'Definition', 'theorem': 'Theorem'};
 
 ['header', 'footer'].forEach(function (name) {
     var content = fs.readFileSync(base_dir + '/views/' + name + '.html', 'utf8');
@@ -21,12 +22,18 @@ app.set('view engine', 'html');
 app.set('views', base_dir + '/views');
 app.use('/static', express.static(base_dir + '/static'));
 
-app.get('/create', function(req, res) {
-  res.render('create', {
-      title: 'Edit Item',
-      extrajs: ['create'],
-      editItemForm: ReactDOMServer.renderToString(<EditItemForm />),
-  });
+app.get('/', function(req, res) {
+    res.render('home');
+});
+app.get('/create/:type', function(req, res) {
+    if (req.params.type in create_types) {
+        res.render('create', {
+            title: 'New ' + create_types[req.params.type],
+            extrajs: ['create'],
+            editItemForm: ReactDOMServer.renderToString(<EditItemForm />),
+        });
+    } else
+        res.sendStatus(404);
 });
 app.get('/show', function(req, res) {
     var data = textToItemData('foobar');
