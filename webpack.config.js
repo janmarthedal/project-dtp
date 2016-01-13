30a4b49d9bc188a77446b3c80539fe86fb13494b
Jan Marthedal Rasmussen <jan@janmr.com>
2016-01-13 08:09:32 +0000

    Tool configs

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /node_modules/,
                 loader: 'babel',
                 query: {
                     presets: ['react', 'es2015'],
