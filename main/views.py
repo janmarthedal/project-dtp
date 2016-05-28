7bc70be668c4fa75ef0854a78c3b177d1e3e4706
Jan Marthedal Rasmussen <jan@janmr.com>
2016-05-28 18:13:10 +0200

    Auth backend logging

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ import requests
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from social.apps.django_app.context_processors import backends as auth_backends
 
 import logging
 logger = logging.getLogger(__name__)
@@ -41,6 +42,9 @@ def login(request):
 @login_required
 def profile(request):
     context = {'title': 'Profile'}
+    backends = auth_backends(request)['backends']
+    logger.info('User {} has providers: {}'.format(request.user.username,
+        ', '.join(p.provider for p in backends['associated'])))
     return render(request, 'main/profile.html', context)
 
 def logout(request):
