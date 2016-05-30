037d92b3be17cfcc28d4eb81ada609453c1a8c9e
Jan Marthedal Rasmussen <jan@janmr.com>
2016-05-30 10:51:00 +0200

    Move JS files into javascript/ folder

--- javascript/gulpfile.js
+++ javascript/gulpfile.js
@@ -2,9 +2,9 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 
 gulp.task('default', () => {
-	return gulp.src('node-src/**/*.js')
+	return gulp.src('src/**/*.js')
 		.pipe(babel({
 			presets: ['es2015']
 		}))
-		.pipe(gulp.dest('node-dst'));
+		.pipe(gulp.dest('dst'));
 });
