18208af5ecc087e962596f636d6492edecf888ea
Jan Marthedal Rasmussen <jan@janmr.com>
2016-10-29 09:04:25 +0200

    Move drafts/views.py to main/views/drafts.py

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 
-from main.views import admin as admin_views, main as main_views
-import drafts.views
+from main.views import (
+    admin as admin_views,
+    main as main_views,
+    drafts as drafts_views
+)
 import mathitems.views
 
 accounts_patterns = [
@@ -11,12 +14,12 @@ accounts_patterns = [
 ]
 
 drafts_patterns = [
-    url(r'^$', drafts.views.list_drafts, name='list-drafts'),
-    url(r'^new/definition$', drafts.views.new_definition, name='new-def'),
-    url(r'^new/theorem$', drafts.views.new_theorem, name='new-thm'),
-    url(r'^new/proof/(T[1-9]\d*)$', drafts.views.new_proof, name='new-prf'),
-    url(r'^(\d+)$', drafts.views.show_draft, name='show-draft'),
-    url(r'^(\d+)/edit$', drafts.views.edit_draft, name='edit-draft'),
+    url(r'^$', drafts_views.list_drafts, name='list-drafts'),
+    url(r'^new/definition$', drafts_views.new_definition, name='new-def'),
+    url(r'^new/theorem$', drafts_views.new_theorem, name='new-thm'),
+    url(r'^new/proof/(T[1-9]\d*)$', drafts_views.new_proof, name='new-prf'),
+    url(r'^(\d+)$', drafts_views.show_draft, name='show-draft'),
+    url(r'^(\d+)/edit$', drafts_views.edit_draft, name='edit-draft'),
 ]
 
 urlpatterns = [
