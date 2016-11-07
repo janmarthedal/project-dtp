d048b5457a5468165e88cd9a485680a65a8d6576
Jan Marthedal Rasmussen <jan@janmr.com>
2016-11-07 12:45:47 +0100

    New datadump

--- main/views/admin.py
+++ main/views/admin.py
@@ -1,36 +1,15 @@
-import re
-from datetime import datetime
-from django.http import JsonResponse
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from django.views.decorators.http import require_safe
 
+from main.item_helpers import item_to_markup
 from mathitems.models import MathItem
-from validations.models import ItemValidation
 
-
-def itemvalidation_data(itemvalidation):
-    return {
-        'type': itemvalidation.source.source_type,
-        'value': itemvalidation.source.source_value,
-        'location': itemvalidation.location
-    }
-
-def mathitem_data(item):
-    result = {
-        'id': item.get_name(),
-        'type': item.item_type,
-        'body': re.sub('[\n]{3,}', '\n\n', item.to_source().strip()),
-        'refs': [itemvalidation_data(itemvalidation)
-                 for itemvalidation in item.itemvalidation_set.all()]
-    }
-    if item.parent:
-        result['parent'] = item.parent.get_name()
-    return result
 
 @require_safe
 def datadump(request):
-    items = [mathitem_data(item) for item in MathItem.objects.order_by('id')]
-    return JsonResponse({
-        'timestamp': datetime.utcnow().isoformat(),
-        'itemCount': len(items),
-        'items': items,
-    })
+    return HttpResponse(''.join(render_to_string('mathitems/dump.txt', {
+        'item': item,
+        'markup': item_to_markup(item),
+        'validations': item.itemvalidation_set.all()
+    }) for item in MathItem.objects.order_by('id')), content_type="text/plain")
