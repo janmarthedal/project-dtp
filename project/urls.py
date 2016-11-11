f07af65969f0c7a449eef37300c0c9fd3848ac5b
Jan Marthedal Rasmussen <jan@janmr.com>
2016-11-11 19:33:49 +0100

    Link to source list page

--- project/urls.py
+++ project/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     url(r'^accounts/', include(accounts_patterns)),
     url(r'^drafts/', include(drafts_patterns)),
     url(r'^datadump$', admin.datadump),
-    url(r'^sources/$', sources.sources_list),
+    url(r'^sources/$', sources.sources_list, name='sources-list'),
     url(r'^concept/([-a-z]+)$', concepts.show_concept),
 ]
