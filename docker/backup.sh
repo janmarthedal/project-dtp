4c63aa0c8f6e43eb52903ce05fcb80512eaa3801
Jan Marthedal Rasmussen <jan@janmr.com>
2016-12-06 13:09:34 +0100

    Update backup script

--- docker/backup.sh
+++ docker/backup.sh
@@ -1,1 +1,2 @@
-docker-compose run --rm app python3 manage.py dumpdata -e sessions -e auth.permission -e contenttypes
+docker-compose run --rm app python3 manage.py dumpdata -e sessions -e auth.permission -e contenttypes \
+  -e concepts.conceptdefinition -e concepts.conceptreference -e concepts.itemdependency -e equations.itemequation
