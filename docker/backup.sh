23fed1594425d4d1b67609cddd9ed633b70b649c
Jan Marthedal Rasmussen <jan@janmr.com>
2017-03-12 19:50:02 +0100

    Backup only own app data

--- docker/backup.sh
+++ docker/backup.sh
@@ -1,2 +1,2 @@
-docker-compose run --rm app python manage.py dumpdata -e sessions -e auth.permission -e contenttypes \
+docker-compose run --rm app python manage.py dumpdata -e contenttypes -e auth -e sessions -e social_django \
   -e concepts.conceptdefinition -e concepts.conceptreference -e concepts.itemdependency -e equations.itemequation
