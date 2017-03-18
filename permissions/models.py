b167011f8da81886191159fe825c2ee458efeadb
Jan Marthedal Rasmussen <jan@janmr.com>
2017-03-18 15:30:04 +0100

    Fix max_length of permissions.Permission.name

--- permissions/models.py
+++ permissions/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 
 class Permission(models.Model):
-    name = models.CharField(max_length=1, unique=True)
+    name = models.CharField(max_length=32, unique=True)
     users = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
