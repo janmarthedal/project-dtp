fb722208c76ede167ca288bc0d6af88a3d3ce6e7
Jan Marthedal Rasmussen <jan@janmr.com>
2017-03-24 16:55:30 +0100

    Update backup script

---
+++ docker/prod/backup.sh
@@ -0,0 +1,11 @@
+docker-compose run --rm app python manage.py dumpdata \
+  -e contenttypes \
+  -e auth \
+  -e sessions \
+  -e social_django \
+  -e concepts.conceptdefinition \
+  -e concepts.conceptreference \
+  -e concepts.itemdependency \
+  -e equations.itemequation \
+  -e concepts.conceptmeta \
+  -e permissions
