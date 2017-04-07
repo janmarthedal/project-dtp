6e16ffdbe83fd5b23aed7db7a65bec7498657881
Jan Marthedal Rasmussen <jan@janmr.com>
2017-04-07 09:57:31 +0200

    Show concept references in item lists

--- main/views/helpers.py
+++ main/views/helpers.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from concepts.models import Concept
 from keywords.models import Keyword
 
@@ -6,9 +8,15 @@ def prepare_item_view_list(item_iterator):
     return [{
         'item': item,
         'defines': list(Concept.objects.filter(conceptdefinition__item=item)
-                            .order_by('name')
-                            .values_list('name', flat=True)),
+                        .distinct()
+                        .order_by('name')
+                        .values_list('name', flat=True)),
+        'refs': list(Concept.objects.exclude(name='*')
+                     .filter(Q(conceptreference__item=item) | Q(itemdependency__item=item))
+                     .distinct()
+                     .order_by('name')
+                     .values_list('name', flat=True)),
         'keywords': list(Keyword.objects.filter(itemkeyword__item=item)
-                            .order_by('name')
-                            .values_list('name', flat=True)),
+                         .order_by('name')
+                         .values_list('name', flat=True)),
     } for item in item_iterator]
