7901373f50b0d740e481fbeb29262a190ae5aaea
Jan Marthedal Rasmussen <jan@janmr.com>
2017-04-21 11:20:36 +0200

    Show publication data for items Introduce data_or_ago template filter

--- main/templatetags/date_utils.py
+++ main/templatetags/date_utils.py
@@ -1,7 +1,18 @@
 from django import template
+from django.utils import timezone
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 register = template.Library()
 
+
 @register.filter(name='toutc')
-def toutc(date):
+def toutc_filter(date):
     return date.replace(tzinfo=None)
+
+
+@register.filter(name='date_or_ago')
+def date_or_ago_filter(date):
+    now = timezone.now()
+    if (now - date).days < 7:
+        return naturaltime(date)
+    return date.strftime('%Y-%m-%d')
--- project/settings.py
+++ project/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize',
     'social_django',
     'pipeline',
 ] + LOCAL_APPS
